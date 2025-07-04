2e5fe0f6576e29e79519104cdbcc3475ced8b878
(rnoon0815) <(msura@naver.com)>
2025-07-04 07:02:18 +0000

    Add BOJ_10828.py to week1/rnoon0815

---
+++ week1/rnoon0815/BOJ_10828.py
@@ -0,0 +1,53 @@
+# 10828 - stack
+
+# list보다 deque 자료구조를 추천함. deque: 양방향 큐 구조, 맨 앞/뒤 삽입과 삭제 모두 O(1)
+from collections import deque
+import sys
+
+Q = deque()
+
+def push(x):
+  Q.append(x)
+  return Q
+
+def pop():
+  if len(Q) == 0: # queue에 아무 것도 들어있지 않을 때
+    print("-1")
+  else:
+    print(Q[-1])
+    Q.pop()
+    return Q
+
+def size(): # 출력 명령
+  length = len(Q)
+  print(length)
+
+def empty(): # 출력 명령
+  if len(Q) == 0:
+    print("1")
+  else:
+    print("0")
+
+def top(): # 출력 명령
+  if len(Q) == 0:
+    print("-1")
+  else:
+    print(Q[-1])
+
+cmd_map = { # msg와 def 맵핑
+    'push':push,
+    'pop':pop,
+    'size':size,
+    'empty':empty,
+    'top':top
+}
+
+n = int(input())
+msgs = sys.stdin.readlines()
+
+for line in msgs:
+  msg = line.strip().split()
+  if len(msg) == 2:
+    cmd_map[msg[0]](msg[1])
+  else:
+    cmd_map[msg[0]]()
