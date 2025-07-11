0f274fb3e135c2e1482b7abe1caad7a45af4a7d0
LeeSH <swhio121@g.skku.du>
2025-07-11 20:55:43 +0900

    sprout

---
+++ week2/skku24cslee/sprout/BOJ_sprout.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "2025-07-11" << endl;
+
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   cout<< "15" << endl << "swho2" << endl;
+
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout <<"\\    /\\" << endl;
+    cout <<" )  ( ')" << endl;
+    cout <<"(  /  )" << endl;
+    cout <<" \\(__)|" << endl;
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "|\\_/|" << endl << "|q p|   /}" << endl << "( 0 )\"\"\"\\" << endl << "|\"^\"`    |" << endl << "||_/=\\\\__|" << endl;
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << R"(         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |)" << endl;
+
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   int a, b;
+   cin >>a >>b;
+   cout << a*b << endl;
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a>> b;
+    cout << a + b << endl << a - b<< endl << a*b << endl << a/b << endl << a%b << endl;
+    return 0;
+}
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >>a >> b;
+    cout << fixed;
+    cout << setprecision(9);
+    cout << a / (double)b <<endl;
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   long int a, b, c;
+   cin >> a >> b >> c;
+   cout << a+ b+ c << endl;
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
